48af2a56200ff303965cf2015c4a870218d7a462
Матин Манафов <matin.manafovvvv@gmail.com>
2021-03-19 18:39:52 +0400

    roles added to systemuser

---
+++ TelecommunicationDevicesStore/TelecommunicationDevicesStore.Domain/Roles/UserRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelecommunicationDevicesStore.Domain.Roles
+{
+	public enum UserRole
+	{
+		Moderator = 1,
+		Admin = 2
+	}
+}
