a5450c71851069cf16cade5d3fc451e5583e9064
Матин Манафов <matin.manafovvvv@gmail.com>
2021-03-19 16:16:39 +0400

    connectionString added migrations started

---
+++ TelecommunicationDevicesStore/TelecommunicationDevicesStore.Domain/Migrations/Configuration.cs
@@ -0,0 +1,20 @@
+﻿namespace TelecommunicationDevicesStore.Domain.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TelecommunicationDevicesStore.Domain.Data.TelecomStoreDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(TelecommunicationDevicesStore.Domain.Data.TelecomStoreDbContext context)
+        {
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+        }
+    }
+}
