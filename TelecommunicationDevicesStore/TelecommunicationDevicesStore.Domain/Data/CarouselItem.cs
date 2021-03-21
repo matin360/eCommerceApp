0427a9fde74dc91151288373c1b2568c48720c1d
Матин Манафов <matin.manafovvvv@gmail.com>
2021-03-21 18:11:34 +0400

    Carousel item added

---
+++ TelecommunicationDevicesStore/TelecommunicationDevicesStore.Domain/Data/CarouselItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelecommunicationDevicesStore.Domain.Data
+{
+	public class CarouselItem
+	{
+		[Required]
+		public int Id { get; set; }
+		[Required]
+		[StringLength(maximumLength: 50, MinimumLength = 3)]
+		public string ImagePath { get; set; }
+		[Required]
+		[StringLength(maximumLength: 50, MinimumLength = 3)]
+		public string Heading { get; set; }
+		[Required]
+		[StringLength(maximumLength: 200, MinimumLength = 10)]
+		public string Description { get; set; }
+		[Required]
+		[StringLength(maximumLength: 50, MinimumLength = 3)]
+		public string TextLink { get; set; }
+		[Required]
+		[StringLength(maximumLength: 50, MinimumLength = 3)]
+		public string ActionLink { get; set; }
+		[Required]
+		[StringLength(maximumLength: 50, MinimumLength = 3)]
+		public string ControllerLink { get; set; }
+	}
+}
