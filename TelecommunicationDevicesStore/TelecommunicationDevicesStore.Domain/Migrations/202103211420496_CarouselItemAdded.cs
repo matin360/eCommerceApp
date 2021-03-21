a47818e305b536780ea0b2dda18810d8ac0e36e8
Матин Манафов <matin.manafovvvv@gmail.com>
2021-03-21 18:26:36 +0400

    CarouselItems table added

---
+++ TelecommunicationDevicesStore/TelecommunicationDevicesStore.Domain/Migrations/202103211420496_CarouselItemAdded.cs
@@ -0,0 +1,30 @@
+﻿namespace TelecommunicationDevicesStore.Domain.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CarouselItemAdded : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+               "dbo.CarouselItems",
+               c => new
+               {
+                   Id = c.Int(nullable: false, identity: true),
+                   Name = c.String(nullable: false, maxLength: 50),
+                   Description = c.String(nullable: false, maxLength: 200),
+                   ImagePath = c.String(nullable: false, maxLength: 50),
+                   Heading = c.String(nullable: false, maxLength: 50),
+                   TextLink = c.String(nullable: false, maxLength: 50),
+                   ActionLink = c.String(nullable: false, maxLength: 50),
+                   ControllerLink = c.String(nullable: false, maxLength: 50),
+               })
+               .PrimaryKey(t => t.Id);
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
