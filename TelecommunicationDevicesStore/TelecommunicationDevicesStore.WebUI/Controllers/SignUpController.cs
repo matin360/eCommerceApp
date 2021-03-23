78b958909d0ca67228a75cb6e33cdbc691adb97a
Матин Манафов <matin.manafovvvv@gmail.com>
2021-03-24 00:18:52 +0400

    signup page added

---
+++ TelecommunicationDevicesStore/TelecommunicationDevicesStore.WebUI/Controllers/SignUpController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TelecommunicationDevicesStore.Domain.Data;
+using TelecommunicationDevicesStore.WebUI.Infrastructure;
+using TelecommunicationDevicesStore.WebUI.Models;
+
+namespace TelecommunicationDevicesStore.WebUI.Controllers
+{
+    public class SignUpController : Controller
+    {
+        private TelecomStoreDbContext _tsdbcontxt;
+        public SignUpController()
+        {
+            _tsdbcontxt = new TelecomStoreDbContext();
+        }
+        // GET: SignUp
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ TelecommunicationDevicesStore/TelecommunicationDevicesStore.WebUI/Models/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TelecommunicationDevicesStore.WebUI.Models
+{
+	public class UserModel
+	{
+		public string UserName { get; set; }
+		[Required]
+		[StringLength(maximumLength: 50)]
+		[EmailAddress]
+		public string Email { get; set; }
+		[Required]
+		[StringLength(maximumLength: 20, MinimumLength = 6)]
+		public string Password { get; set; }
+	}
+}
