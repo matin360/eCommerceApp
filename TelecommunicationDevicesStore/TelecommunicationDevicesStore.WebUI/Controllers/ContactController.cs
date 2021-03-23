132b876c5789b84e0ade77c4af21b7841baa9d62
Матин Манафов <matin.manafovvvv@gmail.com>
2021-03-23 22:55:04 +0400

    contact pge added

---
+++ TelecommunicationDevicesStore/TelecommunicationDevicesStore.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TelecommunicationDevicesStore.Domain.Data;
+using TelecommunicationDevicesStore.WebUI.Infrastructure;
+using TelecommunicationDevicesStore.WebUI.Models;
+
+namespace TelecommunicationDevicesStore.WebUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        private TelecomStoreDbContext _tsdbcontxt;
+        public ContactController()
+        {
+            _tsdbcontxt = new TelecomStoreDbContext();
+        }
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
