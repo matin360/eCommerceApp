bd404883b2169296d5fe041e84a85b49fc7bfeb0
Матин Манафов <matin.manafovvvv@gmail.com>
2021-03-25 20:28:07 +0400

    cart module added

---
+++ TelecommunicationDevicesStore/TelecommunicationDevicesStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TelecommunicationDevicesStore.Domain.Data;
+using TelecommunicationDevicesStore.WebUI.Models;
+
+namespace TelecommunicationDevicesStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private TelecomStoreDbContext _tsdbcontxt;
+        public CartController()
+        {
+            _tsdbcontxt = new TelecomStoreDbContext();
+        }
+        // GET: Cart
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+        public RedirectToRouteResult AddToCart(int productId, string returnUrl)
+        {
+            Product product = _tsdbcontxt.Products
+                .FirstOrDefault(p => p.Id == productId);
+
+            if (product != null)
+            {
+                GetCart().AddItem(product, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
+        {
+            Product product = _tsdbcontxt.Products
+                 .FirstOrDefault(p => p.Id == productId);
+
+            if (product != null)
+            {
+                GetCart().RemoveLine(product);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+    }
+}
---
+++ TelecommunicationDevicesStore/TelecommunicationDevicesStore.WebUI/Models/CartIndexModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TelecommunicationDevicesStore.Domain.Data;
+
+namespace TelecommunicationDevicesStore.WebUI.Models
+{
+	public class CartIndexModel
+	{
+		public Cart Cart { get; set; }
+		public string ReturnUrl { get; set; }
+	}
+}
