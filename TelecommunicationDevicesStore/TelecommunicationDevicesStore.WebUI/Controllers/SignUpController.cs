b6c870739329f23c90e91aa8d8f2272246fce62e
Матин Манафов <matin.manafovvvv@gmail.com>
2021-03-29 14:38:54 +0400

    sing up form completed

--- TelecommunicationDevicesStore/TelecommunicationDevicesStore.WebUI/Controllers/SignUpController.cs
+++ TelecommunicationDevicesStore/TelecommunicationDevicesStore.WebUI/Controllers/SignUpController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using TelecommunicationDevicesStore.Domain.Data;
@@ -25,9 +26,35 @@ namespace TelecommunicationDevicesStore.WebUI.Controllers
         }
 
         [HttpPost]
-        public ActionResult Register(UserModel model)
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(UserModel model)
         {
+			if (ModelState.IsValid)
+			{
+                Customer user = new Customer
+                {
+                    UserName = model.UserName,
+                    Email = model.Email,
+                    Password = model.Password
+                };
+                
+                if(_tsdbcontxt.Customers.Any( x => x.Email == user.Email))
+				{
+                    ModelState.AddModelError("", "User with this email already exists!");
+				}
+				else
+				{
+                    _tsdbcontxt.Customers.Add(user);
+                    await _tsdbcontxt.SaveChangesAsync();
+                    return View(nameof(Completed));
+                }
+			}
             return View();
         }
+
+        public ViewResult Completed()
+		{
+            return View();
+		}
     }
 }
